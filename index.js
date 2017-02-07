93f57e85cbcd26638b33fd734bd4f94a405b06d2
denistsoi <denistsoi@gmail.com>
2017-02-08 01:15:05 +0800

    ADD: WIP stage commit;

--- index.js
+++ index.js
@@ -15,19 +15,95 @@ app.on('ready', ()=> {
   let icon = nativeImage.createFromPath(`${__dirname}/icon@5x.png`)
   tray = new Tray(icon)
 
-
   win = new BrowserWindow(windowSize)
   win.loadURL(`file://${__dirname}/src/index.html`)
+});
 
-})
 
-// const toggleWindow = () => {
-//   if (win.isVisible()) {
-//     win.hide()
-//   } else {
-//     showWindow()
+/**
+ * API End Points
+ */
+// var GIPHY_API_URL = 'http://api.giphy.com/v1/gifs/';
+// var GIPHY_PUBLIC_API_KEY = 'dc6zaTOxFJmzC';
+// var GIPHY_API_KEY = GIPHY_PUBLIC_API_KEY;
+// var GIPHY_TRENDING_URL = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+
+// /**
+//  * Search 
+//  */
+
+// ipcMain.on('search', function(ev, queryString) {
+//   if (!queryString) throw new Error('queryString must be defined', queryString);
+  
+//   var query = 'search?q=' + encodeURIComponent(queryString);
+  
+//   var giphy_url = GIPHY_API_URL + query + '&limit=6' + '&api_key=' + GIPHY_API_KEY;
+//   var popkey_url = {
+//     url: 'https://api.popkey.co/v2/media/' + query + '&page_size=6&page=1',
+//     headers: {
+//       Authorization: process.env.POPKEY_API_KEY
+//     }
 //   }
-// }
 
-// const showWindow = () => {
-// }
+//   request(giphy_url, function(err, response, body) {
+//     mb.window.webContents.send('search:giphy', body);
+//   });
+
+//   request(popkey_url, function(err, response, body) {
+//     mb.window.webContents.send('search:popkey', body);
+//   });
+// });
+
+
+// /**
+//  * Fetch
+//  */
+
+// ipcMain.on('fetch:giphy', function(ev, arg) {
+//   var query = arg.query ? 'search?q=' + encodeURIComponent(arg.query) : '';
+  
+//   if (arg.scope === 'search') {
+//     var giphy_url = GIPHY_API_URL + query  + '&offset=' + arg.offset + '&limit=6' + '&api_key=' + GIPHY_API_KEY;
+//   } else if (arg.scope === 'trending') {
+//     var giphy_url = GIPHY_TRENDING_URL + '&offset=' + arg.offset + '&limit=6';
+//   }
+
+//   request(giphy_url, function(err, response, body) {
+//     mb.window.webContents.send('fetched:giphy', body);
+//   });
+// });
+
+// ipcMain.on('fetch:popkey', function(ev, arg) {
+//   var query = arg.query ? 'search?q=' + encodeURIComponent(arg.query) : '';
+  
+//   if (arg.scope === 'search') {
+//     var popkey_url = {
+//       url: 'https://api.popkey.co/v2/media/' + query + '&page_size=6&page=' + (1+(arg.offset / 6)),
+//       headers: {
+//         Authorization: process.env.POPKEY_API_KEY || 'Basic ZGVza3RvcDplMjFkZDA3ZDlkNTlkYjEwYmJhOGI2ZjI3YmJjNzU5YmQzY2UwNTFiZjhkYmIwNmU1M2Y2MDg1Y2YxN2U5MDhk'
+//       }
+//     }
+//   } else if (arg.scope === 'trending') {
+//     var popkey_url = {
+//       url: 'https://api.popkey.co/v2/media/curated?page_size=6&page=' + (1+(arg.offset / 6)),
+//       headers: {
+//         Authorization: process.env.POPKEY_API_KEY || 'Basic ZGVza3RvcDplMjFkZDA3ZDlkNTlkYjEwYmJhOGI2ZjI3YmJjNzU5YmQzY2UwNTFiZjhkYmIwNmU1M2Y2MDg1Y2YxN2U5MDhk'
+//       }
+//     };
+//   }
+
+//   // request(popkey_url, function(err, response, body) {
+//   //   mb.window.webContents.send('fetched:popkey', body);
+//   // });
+// });
+
+// // const toggleWindow = () => {
+// //   if (win.isVisible()) {
+// //     win.hide()
+// //   } else {
+// //     showWindow()
+// //   }
+// // }
+
+// // const showWindow = () => {
+// // }
